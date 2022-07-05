cbd686f90595bca65eeb51e141f6bcc55cceb942
Sourav <souravsingh9097@gmail.com>
2022-07-05 20:18:20 +0530

    first commit

---
+++ var.tf
@@ -0,0 +1,18 @@
+variable "id" {
+    type = string
+  
+}
+variable "aws_region" {
+    type = string
+  
+}
+
+variable "name" {
+    type = string
+  
+}
+
+variable "dis_name" {
+    type = string
+  
+}
